553fe3a61fd5284a6199f6828cbc937a28251426
mngathers <99606888+mngathers@users.noreply.github.com>
2022-10-27 01:06:47 -0400

    created message form in index.html with a message list, added an event listener on index.js

--- js/index.js
+++ js/index.js
@@ -17,3 +17,44 @@ for (i in skills) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.querySelector('[name="leave_message"]');
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let nameField = document.getElementById("name");
+    let name = nameField.value;
+
+    let emailField = document.getElementById("email");
+    let email = emailField.value;
+
+    let messageField = document.getElementById("message");
+    let message = messageField.value;
+
+    console.log(name, email, message);
+
+    let messageSection = document.getElementById("messages");
+    let messageList = messageSection.querySelector("ul");
+
+    let newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a> <span>wrote: ${message} </span>`;
+    
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.setAttribute("type", "button");
+    removeButton.addEventListener("click", (event) => {
+        let entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    // HIDE MESSAGE SECTION - when list is empty (not working)
+    /*if (messageList.style.display == "none") {
+        messageSection.style.visibility = "hidden";
+    };*/
+
+    messageForm.reset();
+
+});
